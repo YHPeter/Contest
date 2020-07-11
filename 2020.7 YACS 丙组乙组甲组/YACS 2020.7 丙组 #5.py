7c8c711f531781af6c9fdcebca6bb2e367c337e0
unknown <yuhao2003@hotmail.com>
2020-07-11 19:30:24 +0800

    commit to master

--- 2020.7 YACS 丙组乙组甲组/YACS 2020.7 丙组 #5.py
+++ 2020.7 YACS 丙组乙组甲组/YACS 2020.7 丙组 #5.py
@@ -1,30 +1,45 @@
 # YACS 2020.7 丙组 #5
 # 游戏闯关
-
-n,t = map(int,input().split(' '))
-a = map(int,input().strip().split(' '))
-b = map(int,input().strip().split(' '))
-
-dp = [[0]*n for i in range(n)]
-
-
-# a[0] = int(a[0])
-# b[0] = int(b[0])
-# for i in range(1,n):
-#     a[i] = int(a[i])+a[i-1]
-# for i in range(1,n):
-#     b[i] = int(b[i])+b[i-1]
-# print(a,b)
-# c = sorted(a+b)
-# pre = 0
-# for i in range(2*n):
-#     pre+=c[i]
-#     if pre>t:
-#         break
-#         print(i)
-
 '''
 4 22
 6 8 10 7 
 7 11 9 9
+
+return: 3
+
+4 22
+7 8 9 10
+4 5 6 7
+
+return: 4
 '''
+
+n,t = map(int,input().split(' '))
+a = list(map(int,input().strip().split(' ')))
+b = list(map(int,input().strip().split(' ')))
+flaga,flagb = True,True
+for i in range(1,n):
+    if not flaga: pass
+    elif a[i-1]<t: a[i] = a[i]+a[i-1]
+    else:
+        a = a[:i]
+        flaga = False
+
+    if not flagb: pass
+    elif b[i-1]<t: b[i] = b[i]+b[i-1]
+    else:
+        b = b[:i]
+        flagb = False
+
+    if not flaga and not flagb: break
+
+a.insert(0,0)
+b.insert(0,0)
+max_ = 0
+for i in range(1,len(a)):
+    if a[i]>t: continue
+    for j in range(1,len(b)):
+        if a[i]+b[j]<=t: 
+            max_ = max(max_,i+j)
+        else: break
+print(max_)
