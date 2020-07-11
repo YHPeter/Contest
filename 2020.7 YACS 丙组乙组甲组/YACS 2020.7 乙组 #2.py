83485e235f3002b31017457959bfd67d76ddff7d
unknown <yuhao2003@hotmail.com>
2020-07-11 23:37:49 +0800

    commit to master

--- 2020.7 YACS 丙组乙组甲组/YACS 2020.7 乙组 #2.py
+++ 2020.7 YACS 丙组乙组甲组/YACS 2020.7 乙组 #2.py
@@ -1,120 +1,38 @@
 # YACS 2020.5 乙组 #2
-'''
-题目背景
-中国正在以前所未有的力度推进脱贫攻坚，国家计划在2020年使得在现行标准下的贫困人口实现全面脱贫，为此，需要对居民的经济状况进行普查。
-
-题目描述
-给定一个正整数 nn，表示人口数量。一开始，每个居民拥有的财产记为 a_1, a_2, \cdots, a_na 
-1
-​	
- ,a 
-2
-​	
- ,⋯,a 
-n
-​	
- ，财产可能发生两种变化：
-
-一种来源于个体，由某个居民的经济活动而产生。在某个时刻，某个居民的财产会直接变成某个数值，可能升高也可能降低。
-
-一种来源于政策，在某个时刻，国家会对财产低于某个标准线全部的居民，实施政策支持，使得他们的财产全部正好到达标准线。
-
-给定这些变化的详细参数，请做一次统计，输出每个居民最后拥有的财产数量。
+'''小爱有很多 2×2 的积木拼块，现在她打算在一个n×m 的底板上，铺上一些积木拼块，拼块之间不能重合。底板上可能已经存在一些拼块，具体位置由输入给定。请统计小爱有多少种放置拼块的方法。
+举例来说，假设在一个3×4 的蓝色底板上，已经存在一个拼块（以红色表示）：
+01.png
+那么继续放置只有三种可能：
+02.png
+第一种是不放置新的拼块，第二、第三种可能方案是在绿色部分放置一块拼块。
 
 输入格式
-第一行：两个正整数 nn 和 mm；
-第二行：nn个正整数表示 a_1, a_2, \cdots, a_na 
-1
-​	
- ,a 
-2
-​	
- ,⋯,a 
-n
-​	
- ；
-接下来 mm 行：每行依次表示一次财产变化：
+第一行：三个正整数 n，m 与 k；
+接下来 kk 行，每行两个整数 x_i与 y_i ，表示一个已经存在的拼块，(x_i,y_i) 表示该拼块的左上角位置。
 
-以字母 i 开头的表示一次个体变化，后接两个整数参数 uu 和 tt，uu 表示财产发生变化的居民编号，tt 表示财产变化的结果；
-以字母 p 开头的表示一次政策变化，后接一个整数参数 ss，ss 表示政策的标准线。
 输出格式
-共 nn 行：第 ii 行有一个整数，表示 ii 号居民最后拥有的财产数量。
+单个整数：表示放置拼块的方案数。
 
-数据范围
-1\leq s\leq 1000001≤s≤100000；
-1\leq t\leq 1000001≤t≤100000；
-1\leq a_i\leq 1000001≤a 
-i
-​	
- ≤100000；
-对于 30\%30% 的数据，n\leq 1000n≤1000，m\leq 1000m≤1000；
-对于 60\%60% 的数据，n\leq 10000n≤10000，m\leq 10000m≤10000；
-对于 100\%100% 的数据，1\leq n\leq 1000001≤n≤100000，1\leq m\leq 1000001≤m≤100000。
 样例数据
 输入:
-4 3
-10 20 30 40
-p 35
-i 1 20
-p 25
+3 4 1
+2 3
 输出:
-25
-35
-35
-40
-代码提交区域
+3
+
+输入:
+3 4 0
+输出:
+11
+
+标准输出: 128 你的输出: ['8 8 5\n', '2 2\n', '2 5\n', '2 7\n', '6 3\n', '6 6\n']
+准输出: 27 你的输出: ['5 6 2\n', '1 4\n', '2 1\n']
+出: 55 你的输出: ['7 5 3\n', '3 2\n', '4 4\n', '6 4\n']
+准输出: 1084 你的输出: ['7 6 2\n', '1 4\n', '4 1\n']
+标准输出: 15 你的输出: ['6 6 3\n', '2 4\n', '4 1\n', '5 3\n']
+ 7641 你的输出: ['8 7 3\n', '1 1\n', '1 5\n', '6 3\n']
+ 输出: 12 你的输出: ['7 6 5\n', '1 1\n', '2 3\n', '4 1\n', '4 4\n', '6 3\n']
+ 准输出: 1242 你的输出: ['8 7 4\n', '3 3\n', '5 4\n', '6 1\n', '6 6\n']
+ 出: 87 你的输出: ['6 7 4\n', '1 1\n', '1 4\n', '3 4\n', '4 6\n']
+ 准输出: 7641 你的输出: ['8 7 3\n', '1 1\n', '1 5\n', '6 3\n']
 '''
-# n,m = list(map(int,input().split(' '))) # n个人 m次政策和调整
-# citizen = [0]+list(map(int,input().split(' ')))
-# for i in range(m):
-#     a = input().split(' ')
-#     if a[0]=='p':
-#         val = int(a[1])
-#         for i in range(n+1):
-#             citizen[i]=max(citizen[i],val)
-#     elif a[0]=='i':
-#         citizen[int(a[1])] = int(int(a[2]))
-# for x in citizen[1:]: print (x)
-
-
-
-# n,m = list(map(int,input().split(' '))) # n个人 m次政策和调整
-# ini=[0]+list(map(int,input().split(' ')))
-# citizen={}
-# for i in range(1,len(ini)):
-#     citizen[i]=ini[i]
-# # print(citizen)
-# max_p = 0
-# for i in range(m):
-#     a = input().split(' ')
-#     if a[0]=='p':
-#         # max_p = max(max_p,int(a[1]))
-#         for i in citizen.keys():
-#             citizen[i]=max(citizen[i],int(a[1]))
-#     elif a[0]=='i':
-#         citizen[int(a[1])] = int(int(a[2]))
-# for i in citizen.keys():
-#     x = citizen.get(i)
-#     if x==None: print(max_p)
-#     else: print(x)
-
-import sys
-c = sys.stdin.readlines()
-n,m = list(map(int,c[0].strip().split(' '))) 
-ini = [0]+list(map(int,c[1].strip().split(' '))) 
-max_p = 0
-citizen={}
-for i in range(2,m+2):
-    a = c[i].strip().split(' ')
-    if a[0]=='p':
-        for i in citizen.keys():
-            citizen[i]=max(citizen[i],int(a[1]))
-    elif a[0]=='i':
-        citizen[int(a[1])] = int(int(a[2]))
-for i in range(1,n+1):
-    x = citizen.get(i,False)
-    if not x:
-        print(max(max_p,ini[i]))
-    else:
-        print(x)
-
