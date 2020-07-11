1d2d9d94eca9703b294722c686cd461e145a483c
unknown <yuhao2003@hotmail.com>
2020-07-11 18:02:39 +0800

    commit to master

--- 2020.7 YACS 丙组乙组甲组/YACS 2020.7 丙组 #3.py
+++ 2020.7 YACS 丙组乙组甲组/YACS 2020.7 丙组 #3.py
@@ -1,26 +1,9 @@
 # YACS 2020.7 丙组 #3
-# 倍数区间
+# 树根
+x = int(input())
 
-# test: ... out:4
-'''
-4 2
-3 2 4 1
-'''
-
-
-n,k = map(int,input().split(' '))
-a = list(map(lambda x: int(x)%k,input().split(' ')))
-count=0
-arr = [[0]*n for _ in range(n)]
-# print(a)
-for i in range(n):
-    arr[i][i] = a[i]
-    for j in range(i,n):
-        if j!=i:
-            arr[i][j] = (arr[i][j-1]+a[j])%k
-        if arr[i][j]==0: count+=1
-# for i in arr:
-#     print(i)
-print(count)
-
-        
+def f(x):
+    if x<10:
+        return x
+    return f(sum(map(int,list(str(x)))))
+print(f(x))
--- 2020.7 YACS 丙组乙组甲组/YACS 2020.7 丙组 #5.py
+++ 2020.7 YACS 丙组乙组甲组/YACS 2020.7 丙组 #5.py
@@ -1,92 +1,30 @@
-# YACS 2020.5 丙组 #5
-'''
-最大回撤
-内存限制: 256 Mb时间限制: 1000 ms
-题目背景
-股票价格瞬息万变。在金融市场上，经常需要统计一只股票的最大回撤。最大回撤是指投资者在某天买入，在之后的某天卖出，可能造成的最大亏损，它可以反应一只股票在历史上的最坏表现。
+# YACS 2020.7 丙组 #5
+# 游戏闯关
 
-题目描述
-给定一个整数序列 a_1, a_2, a_3, \cdots, a_na 
-1
-​	
- ,a 
-2
-​	
- ,a 
-3
-​	
- ,⋯,a 
-n
-​	
- ，每个 a_ia 
-i
-​	
-  表示某只股票的在某一天的价格，请计算这只股票的最大回撤。即寻找两个下标满足 1\leq i\leq j\leq n1≤i≤j≤n，且 a_i-a_ja 
-i
-​	
- −a 
-j
-​	
- 最大。
+n,t = map(int,input().split(' '))
+a = map(int,input().strip().split(' '))
+b = map(int,input().strip().split(' '))
 
-注意，由于原油宝事件的出现，不能想当然地以为 a_ia 
-i
-​	
-  都是正数。
+dp = [[0]*n for i in range(n)]
 
-输入格式
-第一行：单个整数表示 nn，
-第二行：nn 个整数表示 a_1, a_2, a_3, \cdots, a_na 
-1
-​	
- ,a 
-2
-​	
- ,a 
-3
-​	
- ,⋯,a 
-n
-​	
- 。
 
-输出格式
-单个整数：表示这只股票的最大回撤，如果股票价格一直上涨，输出 0。
-
-数据范围
-对于 30\%30% 的数据，n\leq 1000n≤1000；
-对于 60\%60% 的数据，n\leq 10000n≤10000；
-对于 100\%100% 的数据，1\leq n\leq 1000001≤n≤100000，-100000\leq a_i\leq 100000−100000≤a 
-i
-​	
- ≤100000。
-样例数据
-输入:
-5
-2 3 7 6 1 
-输出:
-6
-说明:
-7-1=6
-输入:
-5
-1 2 3 4 5
-输出:
-0
-说明:
-
+# a[0] = int(a[0])
+# b[0] = int(b[0])
+# for i in range(1,n):
+#     a[i] = int(a[i])+a[i-1]
+# for i in range(1,n):
+#     b[i] = int(b[i])+b[i-1]
+# print(a,b)
+# c = sorted(a+b)
+# pre = 0
+# for i in range(2*n):
+#     pre+=c[i]
+#     if pre>t:
+#         break
+#         print(i)
 
 '''
-def f(prices):
-    if not prices:
-        return 0
-    cur_diff = 0
-    maxn = 0
-    for i in range(1,len(prices)):
-        cur_diff = max(cur_diff+prices[i]-prices[i-1],0)
-        maxn = max(cur_diff,maxn)
-    return maxn
-n = int(input())
-prices = [-int(x) for x in input().split()]
-
-print(f(prices))
+4 22
+6 8 10 7 
+7 11 9 9
+'''
