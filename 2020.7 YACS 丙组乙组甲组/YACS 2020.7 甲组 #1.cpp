2aa59aecf70e8c0b5eea674b2ec3e7b95e0d0340
unknown <yuhao2003@hotmail.com>
2020-07-21 19:51:50 +0800

    commit to master

--- 2020.7 YACS 丙组乙组甲组/YACS 2020.7 甲组 #1.cpp
+++ 2020.7 YACS 丙组乙组甲组/YACS 2020.7 甲组 #1.cpp
@@ -5,8 +5,7 @@ int n,m,k,num[1 << 25];
 long long ans,inv_33 = 1,mod;
 
 void S_search(int dep,int state)
-{
-	if(dep == 0) num[state]++;
+{   if(dep == 0) num[state]++;
 	else for(int i = 1;i <= 26;++i) S_search(dep - 1,((state * 33) ^ i) & mod);
 }
 
@@ -19,7 +18,8 @@ int main()
 {
 	cin>>m>>k>>n;
 	mod = (1 << m) - 1;
-	while(((inv_33 * 33) & mod) != 1) inv_33 = (inv_33 * 33) & mod; 
+	while(((inv_33 * 33) & mod) != 1){
+        inv_33 = (inv_33 * 33) & mod; }
 	S_search(n / 2,0);
 	E_search(n - (n / 2),k);
 	cout<<ans<<endl;
