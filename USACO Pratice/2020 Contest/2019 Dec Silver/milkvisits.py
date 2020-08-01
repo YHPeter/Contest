363c2666f28c9bd689d7e168921ed9381af0d732
unknown <yuhao2003@hotmail.com>
2020-08-01 14:31:24 +0800

    commit to master

--- USACO Pratice/2020 Contest/2019 Dec Silver/milkvisits.py
+++ USACO Pratice/2020 Contest/2019 Dec Silver/milkvisits.py
@@ -5,39 +5,28 @@ TASK: meetings
 """
 problem_name = 'milkvisits'
 
+def find(p):
+    global b
+    if b[p]!=p: b[p] = find(b[p])
+    return b[p]
 
 def solution(row_input):
-    n,m = row_input.pop(0)
-    types = '#'+row_input.pop(0)
-    Hset,Gset = {},{}
-    for i in range(n):
-        x, y = map(int,row_input[i])
-        if types[x]=='H' and types[y]==types[x]:
-            if x in Hset:
-                Hset[x] |= set(y)
-                Hset[y] |= set(x)
-            else:
-                Hset[x] = set(y)
-                Hset[y] = set(x)
-
-        if types[x]=='G' and types[y]==types[x]:
-            if x in Gset:
-                Gset[x] |= set(y)
-                Gset[y] |= set(x)
-            else:
-                Gset[x] = set(y)
-                Gset[y] = set(x)
+    n,m = map(int,row_input.pop(0))
+    c = '#'+row_input.pop(0)[0] # 每个点对应的牛奶品种 str[node]
+    global b
+    b = [i for i in range(n+1)] # [0,1,2,3,4,5,6,7,8...] 初始化node的父节点
     ans = ''
-    for i in range(n,n+m):
-        x,y,c = row_input[i]
-        if c=='H' and y in Hset[x]:
-            ans+='1'
-        elif c=='G' and y in Gset[x]:
-            ans+='1'
+    for i in range(0,n-1):
+        x,y = map(int,row_input[i])
+        if c[x] == c[y]: # 如果两个相连的node，牛一样
+            b[find(x)] = find(y) # 
+    
+    for i in range(n-1,n+m-1):
+        x,y,z = row_input[i]
+        x,y,z = int(x),int(y),z
+        if find(x) != find(y) or c[x] == z: ans+='1'
         else: ans+='0'
-
     return ans
-
 
 # open file
 fin = open ('%s.in'%problem_name, 'r')
