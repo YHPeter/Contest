188152dab0726503ccd483cfddd05f003788d83d
unknown <yuhao2003@hotmail.com>
2020-08-14 21:23:52 +0800

    commit to master

--- CCC Solution/2020/CCC 2020 S2J5.py
+++ CCC Solution/2020/CCC 2020 S2J5.py
@@ -3,16 +3,7 @@ adj = {}
 max_ad = row*column
 import sys
 sys.setrecursionlimit(100000)
-for i in range(1,row+1):
-    x = [0]+list(map(int,input().split()))
-    for j in range(1,column+1):
-        next,cross = x[j],i*j
-        if cross<=max_ad and next<=max_ad:
-            if adj.get(next,False):
-                adj[next].add(cross)
-            else: adj[next] = set([cross])
 
-visited = [max_ad]
 def find(cur_index,visited):
     if cur_index==1:
         print('yes')
@@ -22,5 +13,29 @@ def find(cur_index,visited):
             visited.append(i)
             find(i,visited)
             visited.pop()
+
+for i in range(1,row+1):
+    x = [0]+list(map(int,input().split()))
+    for j in range(1,column+1):
+        next,cross = x[j],i*j
+        if cross<=max_ad and next<=max_ad:
+            if adj.get(next,False):
+                adj[next].add(cross)
+            else: adj[next] = set([cross])
+visited = [max_ad]
 find(max_ad,visited)
 print('no')
+'''
+3
+4
+3 10 8 14
+1 11 12 12
+6 2 3 9
+'''
+
+'''
+2
+2
+1 1
+1 1
+'''
